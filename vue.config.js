a1ca271a51712257dd8796d9af6b890c6371a182
Darwin <serocefinodarwin@gmail.com>
2021-07-26 04:48:54 +0800

    remove

--- vue.config.js
+++ vue.config.js
@@ -1,51 +1,51 @@
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
-const path = require('path');
+// const PrerenderSPAPlugin = require('prerender-spa-plugin')
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+// const path = require('path');
 
 
-module.exports = {
-  pluginOptions: {
-    moment: {
-      locales: [
-        '',
-        'en'
-      ]
-    }
-  },
-  configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return;
-    return {
-      plugins: [
-        new PrerenderSPAPlugin({
-          // The path to the generated file can also be the same as that packaged by webpakc.
-          // The following sentence is very important! ! !
-          // This directory can only have one level. If the directory level is greater than one level, there will be no error prompts when generating, and it will on$
-          staticDir: path.join(__dirname, 'dist'),
+// module.exports = {
+//   pluginOptions: {
+//     moment: {
+//       locales: [
+//         '',
+//         'en'
+//       ]
+//     }
+//   },
+//   configureWebpack: () => {
+//     if (process.env.NODE_ENV !== 'production') return;
+//     return {
+//       plugins: [
+//         new PrerenderSPAPlugin({
+//           // The path to the generated file can also be the same as that packaged by webpakc.
+//           // The following sentence is very important! ! !
+//           // This directory can only have one level. If the directory level is greater than one level, there will be no error prompts when generating, and it will on$
+//           staticDir: path.join(__dirname, 'dist'),
    
-          // Corresponding to your own routing file, for example, a has parameters, you need to write /a/param1.
-          routes: ['/', '/home', '/about', '/c2c'],
+//           // Corresponding to your own routing file, for example, a has parameters, you need to write /a/param1.
+//           routes: ['/', '/home', '/about', '/c2c'],
 
-          // minify: {
-          //   collapseBooleanAttributes: true,
-          //   collapseWhitespace: true,
-          //   decodeEntities: true,
-          //   keepClosingSlash: true,
-          //   sortAttributes: true
-          // },
+//           // minify: {
+//           //   collapseBooleanAttributes: true,
+//           //   collapseWhitespace: true,
+//           //   decodeEntities: true,
+//           //   keepClosingSlash: true,
+//           //   sortAttributes: true
+//           // },
    
-          // This is very important, if it is not configured, it will not be precompiled.
-          renderer: new Renderer({
+//           // This is very important, if it is not configured, it will not be precompiled.
+//           renderer: new Renderer({
             
-          //   inject: {
-          //       foo: 'bar'      
-          //   },
-          //  headless: false,
-          //  renderAfterTime: 5000,
-            // In main.js document.dispatchEvent(new Event('render-event')), the event names of the two should correspond.
-            renderAfterDocumentEvent: 'render-event'
-          })
-        })
-      ]
-    };
-  },
-};
+//           //   inject: {
+//           //       foo: 'bar'      
+//           //   },
+//           //  headless: false,
+//           //  renderAfterTime: 5000,
+//             // In main.js document.dispatchEvent(new Event('render-event')), the event names of the two should correspond.
+//             renderAfterDocumentEvent: 'render-event'
+//           })
+//         })
+//       ]
+//     };
+//   },
+// };
