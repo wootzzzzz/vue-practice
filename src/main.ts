29c2c40d93316b2ae9ecef202a790a72fdb8ac38
Darwin <serocefinodarwin@gmail.com>
2021-07-26 17:02:11 +0800

    remove prerender

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
-  // mounted() {
-  //   document.dispatchEvent(new Event('render-event'));
-  // }
+  mounted() {
+    document.dispatchEvent(new Event('render-event'));
+  }
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -8,52 +8,29 @@ module.exports = {
           '',
           'en'
         ]
-      },
-      prerenderSpa: {
-        registry: undefined,
-        renderRoutes: [
-          '/',
-          '/home',
-          '/c2c'
-        ],
-        useRenderEvent: true,
-        onlyProduction: true,
-        
-        headless: false, // <- this could also be inside the customRendererConfig
-        customRendererConfig: 
-        {
-          args: ["--auto-open-devtools-for-tabs"]
-        },
-        postProcess: route => {
-          // Defer scripts and tell Vue it's been server rendered to trigger hydration
-          route.html = route.html
-            .replace(/<script (.*?)>/g, '<script $1 defer>')
-            .replace('id="app"', 'id="app" data-server-rendered="true"');
-          return route;
-        }
       }
     },
-    // configureWebpack: () => {
-    //   if (process.env.NODE_ENV !== 'production') return;
-    //   return {
-    //     plugins: [
-    //       new PrerenderSPAPlugin({
-    //         // The path to the generated file can also be the same as that packaged by webpakc.
-    //         // The following sentence is very important! ! !
-    //         // This directory can only have one level. If the directory level is greater than one level, there will be no error prompts when generating, and it will on$
-    //         staticDir: path.join(__dirname, 'dist'),
+    configureWebpack: () => {
+      if (process.env.NODE_ENV !== 'production') return;
+      return {
+        plugins: [
+          new PrerenderSPAPlugin({
+            // The path to the generated file can also be the same as that packaged by webpakc.
+            // The following sentence is very important! ! !
+            // This directory can only have one level. If the directory level is greater than one level, there will be no error prompts when generating, and it will on$
+            staticDir: path.join(__dirname, 'dist'),
      
-    //         // Corresponding to your own routing file, for example, a has parameters, you need to write /a/param1.
-    //         routes: ['/', '/home', '/c2c/'],
+            // Corresponding to your own routing file, for example, a has parameters, you need to write /a/param1.
+            routes: ['/', '/home', '/c2c/'],
      
-    //         // This is very important, if it is not configured, it will not be precompiled.
-    //         renderer: new Renderer({
-    //           // In main.js document.dispatchEvent(new Event('render-event')), the event names of the two should correspond.
-    //           renderAfterDocumentEvent: 'render-event'
-    //         })
-    //       })
-    //     ]
-    //   };
-    // },
+            // This is very important, if it is not configured, it will not be precompiled.
+            renderer: new Renderer({
+              // In main.js document.dispatchEvent(new Event('render-event')), the event names of the two should correspond.
+              renderAfterDocumentEvent: 'render-event'
+            })
+          })
+        ]
+      };
+    },
   };
   
