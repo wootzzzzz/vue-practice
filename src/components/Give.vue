c5b0bed271386da4a8359bb491323c4a1f4ac409
Darwin <serocefinodarwin@gmail.com>
2021-07-27 13:08:21 +0800

    add twitter

--- src/components/Give.vue
+++ src/components/Give.vue
@@ -27,7 +27,12 @@ metaInfo() {
         { property: 'og:type', content: 'website'},
         { property: 'og:site_name', content: 'Baseco Tondo'},
         // { property: 'og:url', content: process.env.VUE_APP_BASE_URL + '/' + this.$route.params.link },
-        { property: 'og:image', content: 'https://api.heropoints.tech/public/upload/ngo/1/1/form_image.jpeg' }    
+        { property: 'og:image', content: 'https://api.heropoints.tech/public/upload/ngo/1/1/form_image.jpeg' }, 
+        { property: "twitter:card", content:"summary"},
+        { property: "twitter:url", content:"https://loftie.com"},
+        { property: "twitter:title", content:"Vue Social Cards Example"},
+        { property: "twitter:description", content:"Vue sample site showing off Twitter and Facebook Cards."},
+        { property: "twitter:image", content:"http://dev.to/social_previews/user/139093.png"}
       ]
     };
   },
